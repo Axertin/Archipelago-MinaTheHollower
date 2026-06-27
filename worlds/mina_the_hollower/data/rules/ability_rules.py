a4a1de6cea67d419f2bd64c91a3ffe8fd269591f
FyreDay <fyredaymc@gmail.com>
2026-06-27 11:47:00 -0500

    update reaching sidearm to require joules

--- worlds/mina_the_hollower/data/rules/ability_rules.py
+++ worlds/mina_the_hollower/data/rules/ability_rules.py
@@ -13,7 +13,7 @@ from ...options import AbilityRando
 from ...world_base import MinaTheHollowerBase
 
 def HasReachingSideArm():
-    return True_()
+    return Has(PlayerUpgrades.JOULE_BOX.value)
 
 @dataclasses.dataclass(kw_only=True)
 class CanBurrow(Rule[MinaTheHollowerBase], game=MINA_THE_HOLLOWER):
