3be6b06d73e261b90a0de655a08fc31f14727954
FyreDay <44561044+FyreDay@users.noreply.github.com>
2026-08-09 21:40:59 -0500

    Apply suggestion from @FyreDay

--- worlds/mina_the_hollower/data/locations/areas/loners_landing.py
+++ worlds/mina_the_hollower/data/locations/areas/loners_landing.py
@@ -15,7 +15,7 @@ collectable_locations: dict[str, LocationData] = {
     "LL Boardwalk Fire Bounce Chest": LocationData(27, "Loner's Landing Boardwalk Fire Bounce", CanBounce()),
     "LL Boardwalk Sandwater Ledge Chest": LocationData(324, "Loner's Landing Boardwalk Sandfalls Ledge", item_rule=lambda item: item.name != Wallets.WALLET_SIZE.value),
     "LL Fish Trigger Antennae": LocationData(32, "Loner's Landing Boat Side", HasFishingRod() & CanBurrow()),
-    "LL Belowdecks Left Unchosen Weapon": LocationData(17, "Loner's Landing Belowdecks", HasKear(kear=SingleKears.LONERS_LANDING_BELOWDECKS_LEFT_WEAPON_KEAR.value) & HasKear(kear=SingleKears.LONERS_LANDING_BELOWDECKS_BACK_KEAR.value)),
+    "LL Belowdecks Left Unchosen Weapon": LocationData(17, "Loner's Landing Belowdecks Chests", HasKear(kear=SingleKears.LONERS_LANDING_BELOWDECKS_LEFT_WEAPON_KEAR.value)),
     "LL Belowdecks Right Unchosen Weapon": LocationData(18, "Loner's Landing Belowdecks", HasKear(kear=SingleKears.LONERS_LANDING_BELOWDECKS_RIGHT_WEAPON_KEAR.value) & HasKear(kear=SingleKears.LONERS_LANDING_BELOWDECKS_BACK_KEAR.value)),
     "LL Belowdecks Chest": LocationData(30, "Loner's Landing Belowdecks", CanJumpTiles(distance=2)),
 }
